55ed10e05a5c46ca48f4d4d48d5930bccfed53b6
mohitmarshal2006-hash <mohitmarshal2006@gmail.com>
2026-05-28 08:01:53 +0530

    Update and rename README.md to Servercode.py

---
+++ Servercode.py
@@ -0,0 +1,12 @@
+import socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("127.0.0.1", 12345))
+server_socket.listen(1)
+print("Server is waiting...")
+conn, addr = server_socket.accept()
+print("Connected by", addr)
+data = conn.recv(1024)
+print("Client says:", data.decode())
+conn.send("Hello Client!".encode())
+conn.close()
+server_socket.close()
